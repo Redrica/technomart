ab08f27967f1da89e7db4f6279ccad713f9ca8cf
RomanKostyrin <enemy-iubip@mail.ru>
2019-12-08 12:29:26 +0300

    исправил JS. работают все попапы

--- js/script.js
+++ js/script.js
@@ -1,62 +1,96 @@
 
-let link = document.querySelector(".btn--contacts");
-let popup = document.querySelector(".write-us");
-let close = document.querySelector(".btn-close");
-let closeMap = document.querySelector(".btn-close--map");
-let form = popup.querySelector(".write-us__form");
-let login = popup.querySelector("[name=login]");
-let email = popup.querySelector("[name=email]");
-let comment = popup.querySelector("[name=commeny]");
-let mapLink = document.querySelector(".map-link");
-let map = document.querySelector(".map");
-let buy = document.querySelector(".btn--shop-item");
-let cart = document.querySelector(".cart");
+var link = document.querySelector(".btn--contacts");
+var popup = document.querySelector(".write-us");
+var close = document.querySelector(".btn--close");
+var form = document.querySelector(".write-us__form");
+var login = document.querySelector("[name=login]");
+var email = document.querySelector("[name=email]");
+var comment = document.querySelector("[name=commeny]");
+var mapLink = document.querySelector(".map-link");
+var map = document.querySelector(".map");
+var buy = document.querySelector(".btn--shop-item");
+var cart = document.querySelector(".cart");
 
-link.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.add("popup--show");
-});
-
-buy.addEventListener("click", function(event) {
-event.preventDefault();
-cart.classList.add("popup--show");
-});
-
-//При нажатии крестика закрывается окно
-close.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.remove("popup--show");
-popup.classList.remove("popup--error");
-});
-
-//При нажатии крестика закрывается окно
-closeMap.addEventListener("click", function(event) {
-event.preventDefault();
-map.classList.remove("popup--show");
-});
-
-//При нажатии сабмит сразатывает ошибка
-form.addEventListener("submit", function(event) {
-if (!login.value || !email.value || !comment.value) {
-event.preventDefault();
-popup.classList.add("popup--error");
+if (link) {
+	link.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("popup--show");
+	});
 }
-});
+if (mapLink) {
+	mapLink.addEventListener("click", function(event) {
+	event.preventDefault();
+	map.classList.add("popup--show");
+	});
+}
+//При нажатии крестика закрывается окно
+let closes = document.querySelectorAll(".btn-close");
+for (let btnClose of closes) {
+  btnClose.onclick = function(event) {
 
+	if (popup) {
+		if (popup.classList.contains("popup--show")) {
+			event.preventDefault();
+			popup.classList.remove("popup--show");
+			popup.classList.remove("popup--error");		
+			}
+		}
+		if (map) {
+			if (map.classList.contains("popup--show")) {
+				event.preventDefault();
+				map.classList.remove("popup--show");
+			}
+		}
+		if (cart) {
+			if (cart.classList.contains("popup--show")) {
+				event.preventDefault();
+				cart.classList.remove("popup--show");
+			}
+		}
+	}
+}
+//При нажатии сабмит срабатывает ошибка
+if (form) {
+	form.addEventListener("submit", function(event) {
+	if (!login.value || !email.value || !comment.value) {
+	event.preventDefault();
+	popup.classList.add("popup--error");
+	}
+	});
+}
 //При нажатии Esc закрывается окно
-window.addEventListener("keydown", function(event) {
-if (event.keyCode === 27) {
-if (popup.classList.contains("popup--show")) {
-popup.classList.remove("popup--show");
-}
-if (map.classList.contains("popup--show")) {
-map.classList.remove("popup--show");
-}
-}
-});
+	window.addEventListener("keydown", function(event) {
+	if (event.keyCode === 27) {
+		if (popup) {
+			if (popup.classList.contains("popup--show")) {
+			popup.classList.remove("popup--show");
+			}
+		}
+		if (map) {
+			if (map.classList.contains("popup--show")) {
+			map.classList.remove("popup--show");
+			}
+		}
+		if (cart) {
+			if (cart.classList.contains("popup--show")) {
+			cart.classList.remove("popup--show");
+			}
+		}
+	}
+	});
 
-//Открываем карту расширенную 
-mapLink.addEventListener("click", function(event) {
-event.preventDefault();
-map.classList.add("popup--show");
-});
+let buys = document.querySelectorAll(".btn--shop-item");
+for (let btnShop of buys) {
+  btnShop.onclick = function(event) {
+		if (buy) {
+			event.preventDefault();
+			cart.classList.add("popup--show");
+		}
+  }
+}
+if (cart) {
+	close.addEventListener("click", function(event) {
+	event.preventDefault();
+	cart.classList.remove("popup--show");
+	});
+}
